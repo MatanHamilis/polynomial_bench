b40d521b58c5506fca36f92fbeb022fe99639450
Elichai Turkel <elichai.turkel@gmail.com>
2025-02-26 12:48:38 +0200

    Add ReversedPolynomial

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,7 +1,8 @@
 use criterion::{Criterion, black_box, criterion_group, criterion_main};
 fn bench(c: &mut Criterion) {
     let polynomial = benching_polynomials::Polynomial::random(10_000);
-    let evals = (0..100).collect::<Vec<_>>();
+    let reversed_polynomial = polynomial.clone().reverse();
+    let evals: Vec<u64> = (0..100).collect::<Vec<_>>();
     c.bench_function("regular", |b| {
         b.iter(|| {
             let (poly, evals) = black_box((&polynomial, evals.as_slice()));
@@ -13,6 +14,23 @@ fn bench(c: &mut Criterion) {
     c.bench_function("horner", |b| {
         b.iter(|| {
             let (poly, evals) = black_box((&polynomial, evals.as_slice()));
+            for &x in evals {
+                black_box(poly.eval_horner(x));
+            }
+        })
+    });
+
+    c.bench_function("reversed regular", |b| {
+        b.iter(|| {
+            let (poly, evals) = black_box((&reversed_polynomial, evals.as_slice()));
+            for &x in evals {
+                black_box(poly.eval(x));
+            }
+        })
+    });
+    c.bench_function("reversed horner", |b| {
+        b.iter(|| {
+            let (poly, evals) = black_box((&reversed_polynomial, evals.as_slice()));
             for &x in evals {
                 black_box(poly.eval_horner(x));
             }
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,36 @@
 use std::num::Wrapping;
 
 // Polynomial in Z_{2^64}.
+#[derive(Clone)]
 pub struct Polynomial {
     coefficients: Vec<Wrapping<u64>>,
 }
 
+fn eval_naive(x: Wrapping<u64>, coeff: impl Iterator<Item = Wrapping<u64>>) -> Wrapping<u64> {
+    let mut result = Wrapping(0);
+    let mut pow = Wrapping(1);
+    for coeff in coeff {
+        result += coeff * pow;
+        pow *= x;
+    }
+    result
+}
+
+// pass reversed coefficients
+fn eval_horner(x: Wrapping<u64>, coeff: impl Iterator<Item = Wrapping<u64>>) -> Wrapping<u64> {
+    let mut result = Wrapping(0);
+    for coeff in coeff {
+        result = result * x + coeff;
+    }
+    result
+}
+
 impl Polynomial {
     pub fn eval(&self, x: u64) -> u64 {
-        let mut result = Wrapping(0);
-        let mut pow = Wrapping(1);
-        for &coeff in &self.coefficients {
-            result += coeff * pow;
-            pow *= x;
-        }
-        result.0
+        eval_naive(Wrapping(x), self.coefficients.iter().copied()).0
     }
     pub fn eval_horner(&self, x: u64) -> u64 {
-        let mut result = Wrapping(0);
-        let x = Wrapping(x);
-        for &coeff in self.coefficients.iter().rev() {
-            result = result * x + coeff;
-        }
-        result.0
+        eval_horner(Wrapping(x), self.coefficients.iter().rev().copied()).0
     }
     pub fn random(degree: usize) -> Polynomial {
         let mut coefficients = Vec::with_capacity(degree);
@@ -29,6 +38,41 @@ impl Polynomial {
             coefficients.push(rand::random());
         }
         Polynomial { coefficients }
+    }
+
+    pub fn reverse(mut self) -> ReversedPolynomial {
+        self.coefficients.reverse();
+        ReversedPolynomial {
+            coefficients: self.coefficients,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct ReversedPolynomial {
+    coefficients: Vec<Wrapping<u64>>,
+}
+
+impl ReversedPolynomial {
+    pub fn eval(&self, x: u64) -> u64 {
+        eval_horner(Wrapping(x), self.coefficients.iter().rev().copied()).0
+    }
+    pub fn eval_horner(&self, x: u64) -> u64 {
+        eval_naive(Wrapping(x), self.coefficients.iter().copied()).0
+    }
+    pub fn random(degree: usize) -> Polynomial {
+        let mut coefficients = Vec::with_capacity(degree);
+        for _ in 0..degree {
+            coefficients.push(rand::random());
+        }
+        Polynomial { coefficients }
+    }
+
+    pub fn reverse(mut self) -> Polynomial {
+        self.coefficients.reverse();
+        Polynomial {
+            coefficients: self.coefficients,
+        }
     }
 }
 
