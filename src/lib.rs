e4f9f100ebcd7a02da79041ac0920f4323b939b0
Elichai Turkel <elichai.turkel@gmail.com>
2025-02-26 12:47:36 +0200

    Make polynomial compute Wrapping

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,27 @@
+use std::num::Wrapping;
+
 // Polynomial in Z_{2^64}.
 pub struct Polynomial {
-    coefficients: Vec<u64>,
+    coefficients: Vec<Wrapping<u64>>,
 }
 
 impl Polynomial {
     pub fn eval(&self, x: u64) -> u64 {
-        let mut result = 0;
-        let mut pow = 1;
+        let mut result = Wrapping(0);
+        let mut pow = Wrapping(1);
         for &coeff in &self.coefficients {
             result += coeff * pow;
-            pow = unsafe { pow.unchecked_mul(x) };
+            pow *= x;
         }
-        result
+        result.0
     }
     pub fn eval_horner(&self, x: u64) -> u64 {
-        let mut result = 0;
+        let mut result = Wrapping(0);
+        let x = Wrapping(x);
         for &coeff in self.coefficients.iter().rev() {
             result = result * x + coeff;
         }
-        result
+        result.0
     }
     pub fn random(degree: usize) -> Polynomial {
         let mut coefficients = Vec::with_capacity(degree);
