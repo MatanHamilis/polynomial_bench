9ff64f082eb2477e739d0d782e0d717eea6ebdb5
Elichai Turkel <elichai.turkel@gmail.com>
2025-02-26 12:38:23 +0200

    Make athe benchmark loop over 100 evals, and use black_box to prevent const propogation

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,15 +1,21 @@
-use criterion::{Criterion, criterion_group, criterion_main};
+use criterion::{Criterion, black_box, criterion_group, criterion_main};
 fn bench(c: &mut Criterion) {
+    let polynomial = benching_polynomials::Polynomial::random(10_000);
+    let evals = (0..100).collect::<Vec<_>>();
     c.bench_function("regular", |b| {
-        let polynomial = benching_polynomials::Polynomial::random(10_000);
         b.iter(|| {
-            polynomial.eval(3);
+            let (poly, evals) = black_box((&polynomial, evals.as_slice()));
+            for &x in evals {
+                black_box(poly.eval(x));
+            }
         })
     });
     c.bench_function("horner", |b| {
-        let polynomial = benching_polynomials::Polynomial::random(10_000);
         b.iter(|| {
-            polynomial.eval_horner(3);
+            let (poly, evals) = black_box((&polynomial, evals.as_slice()));
+            for &x in evals {
+                black_box(poly.eval_horner(x));
+            }
         })
     });
 }
