a631e29f51a209828bbc72578061a1238eff75ef
Elichai Turkel <elichai.turkel@gmail.com>
2025-02-26 13:45:14 +0200

    Generalize the library and benchmark multiple types

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,42 +1,66 @@
+use benching_polynomials::{Polynomial, ScalarTrait};
 use criterion::{Criterion, black_box, criterion_group, criterion_main};
-fn bench(c: &mut Criterion) {
-    let polynomial = benching_polynomials::Polynomial::random(10_000);
+use std::{iter::Sum, num::Wrapping};
+
+const DEGREE: usize = 10_000;
+const EVALS: usize = 64;
+
+fn bench_for_type<S: ScalarTrait + Sum>(c: &mut Criterion, type_name: &str) {
+    let polynomial = Polynomial::<S>::random(DEGREE);
     let reversed_polynomial = polynomial.clone().reverse();
-    let evals: Vec<u64> = (0..100).collect::<Vec<_>>();
-    c.bench_function("regular", |b| {
+    let mut rng = rand::rng();
+    let evals = (0..EVALS).map(|_| S::rand(&mut rng)).collect::<Vec<_>>();
+
+    let mut group = c.benchmark_group(type_name);
+
+    // each test passes the polynomial and the evaluations through a black_box to prevent the optimizer
+    // from knowing anything about them.
+    // we then sum the result and "return" them which will also be passed to a `black_box` by criterion
+    // this prevents dead-code elimination and ensures that the benchmark is actually measuring the
+    // time it takes to compute the sum of the evaluations. (and the addition there is very negligible)
+    group.bench_function("regular", |b| {
         b.iter(|| {
-            let (poly, evals) = black_box((&polynomial, evals.as_slice()));
-            for &x in evals {
-                black_box(poly.eval(x));
-            }
+            let (poly, evals) = black_box((&polynomial, evals.iter().copied()));
+            evals.map(|e| poly.eval(e)).sum::<S>()
         })
     });
-    c.bench_function("horner", |b| {
+    group.bench_function("horner", |b| {
         b.iter(|| {
-            let (poly, evals) = black_box((&polynomial, evals.as_slice()));
-            for &x in evals {
-                black_box(poly.eval_horner(x));
-            }
+            let (poly, evals) = black_box((&polynomial, evals.iter().copied()));
+            evals.map(|e| poly.eval_horner(e)).sum::<S>()
+        })
+    });
+    group.bench_function("reversed regular", |b| {
+        b.iter(|| {
+            let (poly, evals) = black_box((&reversed_polynomial, evals.iter().copied()));
+            evals.map(|e| poly.reverse_eval(e)).sum::<S>()
+        })
+    });
+    group.bench_function("reversed horner", |b| {
+        b.iter(|| {
+            let (poly, evals) = black_box((&reversed_polynomial, evals.iter().copied()));
+            evals.map(|e| poly.reverse_eval_horner(e)).sum::<S>()
         })
     });
 
-    c.bench_function("reversed regular", |b| {
-        b.iter(|| {
-            let (poly, evals) = black_box((&reversed_polynomial, evals.as_slice()));
-            for &x in evals {
-                black_box(poly.eval(x));
-            }
-        })
-    });
-    c.bench_function("reversed horner", |b| {
-        b.iter(|| {
-            let (poly, evals) = black_box((&reversed_polynomial, evals.as_slice()));
-            for &x in evals {
-                black_box(poly.eval_horner(x));
-            }
-        })
-    });
+    group.finish();
 }
 
-criterion_group!(benches, bench);
+fn bench_u64(c: &mut Criterion) {
+    bench_for_type::<Wrapping<u64>>(c, "u64");
+}
+
+fn bench_u128(c: &mut Criterion) {
+    bench_for_type::<Wrapping<u128>>(c, "u128");
+}
+
+fn bench_k256(c: &mut Criterion) {
+    bench_for_type::<k256::Scalar>(c, "k256::Scalar");
+}
+
+fn bench_curve25519(c: &mut Criterion) {
+    bench_for_type::<curve25519_dalek::Scalar>(c, "curve25519_dalek::Scalar");
+}
+
+criterion_group!(benches, bench_u64, bench_u128, bench_k256, bench_curve25519);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,27 @@
-use std::num::Wrapping;
+use k256::elliptic_curve::PrimeField;
+use rand::Rng;
+use std::{
+    num::Wrapping,
+    ops::{Add, AddAssign, Mul, MulAssign},
+};
 
-// Polynomial in Z_{2^64}.
-#[derive(Clone)]
-pub struct Polynomial {
-    coefficients: Vec<Wrapping<u64>>,
+pub trait ScalarTrait:
+    Sized + Add<Output = Self> + AddAssign + Mul<Output = Self> + MulAssign + Copy
+{
+    const ZERO: Self;
+    const ONE: Self;
+    fn rand(r: &mut impl rand::RngCore) -> Self;
 }
 
-fn eval_naive(x: Wrapping<u64>, coeff: impl Iterator<Item = Wrapping<u64>>) -> Wrapping<u64> {
-    let mut result = Wrapping(0);
-    let mut pow = Wrapping(1);
+// Polynomial in Z_{2^64}.
+#[derive(Clone, Debug)]
+pub struct Polynomial<S: ScalarTrait> {
+    coefficients: Vec<S>,
+}
+
+fn eval_naive<S: ScalarTrait>(x: S, coeff: impl Iterator<Item = S>) -> S {
+    let mut result = S::ZERO;
+    let mut pow = S::ONE;
     for coeff in coeff {
         result += coeff * pow;
         pow *= x;
@@ -17,62 +30,71 @@ fn eval_naive(x: Wrapping<u64>, coeff: impl Iterator<Item = Wrapping<u64>>) -> W
 }
 
 // pass reversed coefficients
-fn eval_horner(x: Wrapping<u64>, coeff: impl Iterator<Item = Wrapping<u64>>) -> Wrapping<u64> {
-    let mut result = Wrapping(0);
+fn eval_horner<S: ScalarTrait>(x: S, coeff: impl Iterator<Item = S>) -> S {
+    let mut result = S::ZERO;
     for coeff in coeff {
         result = result * x + coeff;
     }
     result
 }
 
-impl Polynomial {
-    pub fn eval(&self, x: u64) -> u64 {
-        eval_naive(Wrapping(x), self.coefficients.iter().copied()).0
+impl<S: ScalarTrait> Polynomial<S> {
+    pub fn eval(&self, x: S) -> S {
+        eval_naive(x.into(), self.coefficients.iter().copied())
     }
-    pub fn eval_horner(&self, x: u64) -> u64 {
-        eval_horner(Wrapping(x), self.coefficients.iter().rev().copied()).0
+    pub fn eval_horner(&self, x: S) -> S {
+        eval_horner(x.into(), self.coefficients.iter().rev().copied())
     }
-    pub fn random(degree: usize) -> Polynomial {
+    pub fn reverse_eval(&self, x: S) -> S {
+        eval_naive(x, self.coefficients.iter().rev().copied())
+    }
+    pub fn reverse_eval_horner(&self, x: S) -> S {
+        eval_horner(x, self.coefficients.iter().copied())
+    }
+    pub fn random(degree: usize) -> Self {
         let mut coefficients = Vec::with_capacity(degree);
+        let mut rng = rand::rng();
         for _ in 0..degree {
-            coefficients.push(rand::random());
+            coefficients.push(S::rand(&mut rng));
         }
         Polynomial { coefficients }
     }
 
-    pub fn reverse(mut self) -> ReversedPolynomial {
+    pub fn reverse(mut self) -> Self {
         self.coefficients.reverse();
-        ReversedPolynomial {
-            coefficients: self.coefficients,
-        }
+        self
     }
 }
 
-#[derive(Clone)]
-pub struct ReversedPolynomial {
-    coefficients: Vec<Wrapping<u64>>,
+impl ScalarTrait for Wrapping<u64> {
+    const ZERO: Self = Wrapping(0);
+    const ONE: Self = Wrapping(1);
+    fn rand(r: &mut impl rand::RngCore) -> Self {
+        r.random()
+    }
 }
 
-impl ReversedPolynomial {
-    pub fn eval(&self, x: u64) -> u64 {
-        eval_horner(Wrapping(x), self.coefficients.iter().rev().copied()).0
+impl ScalarTrait for Wrapping<u128> {
+    const ZERO: Self = Wrapping(0);
+    const ONE: Self = Wrapping(1);
+    fn rand(r: &mut impl rand::RngCore) -> Self {
+        r.random()
     }
-    pub fn eval_horner(&self, x: u64) -> u64 {
-        eval_naive(Wrapping(x), self.coefficients.iter().copied()).0
-    }
-    pub fn random(degree: usize) -> Polynomial {
-        let mut coefficients = Vec::with_capacity(degree);
-        for _ in 0..degree {
-            coefficients.push(rand::random());
-        }
-        Polynomial { coefficients }
-    }
+}
 
-    pub fn reverse(mut self) -> Polynomial {
-        self.coefficients.reverse();
-        Polynomial {
-            coefficients: self.coefficients,
-        }
+impl ScalarTrait for k256::Scalar {
+    const ZERO: Self = Self::ZERO;
+    const ONE: Self = Self::ONE;
+    fn rand(r: &mut impl rand::RngCore) -> Self {
+        Self::from_repr_vartime(r.random::<[u8; 32]>().into()).unwrap()
+    }
+}
+
+impl ScalarTrait for curve25519_dalek::Scalar {
+    const ZERO: Self = Self::ZERO;
+    const ONE: Self = Self::ONE;
+    fn rand(r: &mut impl rand::RngCore) -> Self {
+        Self::from_bytes_mod_order_wide(&r.random::<[u8; 64]>())
     }
 }
 
@@ -82,9 +104,9 @@ fn test_poly_eval() {
     let mut rng = rand::rng();
     for _ in 0..100 {
         let degree: u16 = rng.random();
-        let poly = Polynomial::random(degree as usize);
+        let poly = Polynomial::<Wrapping<u64>>::random(degree as usize);
         for _ in 0..100 {
-            let x: u64 = rng.random();
+            let x = rng.random();
             assert_eq!(poly.eval(x), poly.eval_horner(x));
         }
     }
