1fcbd6e1fd4b207d4362cf2b92f14dcfc5225ccc
Elichai Turkel <elichai.turkel@gmail.com>
2025-02-26 12:45:17 +0200

    Add eval test

--- src/lib.rs
+++ src/lib.rs
@@ -28,3 +28,17 @@ impl Polynomial {
         Polynomial { coefficients }
     }
 }
+
+#[test]
+fn test_poly_eval() {
+    use rand::Rng;
+    let mut rng = rand::rng();
+    for _ in 0..100 {
+        let degree: u16 = rng.random();
+        let poly = Polynomial::random(degree as usize);
+        for _ in 0..100 {
+            let x: u64 = rng.random();
+            assert_eq!(poly.eval(x), poly.eval_horner(x));
+        }
+    }
+}
